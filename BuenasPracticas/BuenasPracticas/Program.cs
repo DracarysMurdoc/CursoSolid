ceee2ce44f0ffb9935cb568aed26e3e6dc7158d6
DracarysMurdoc <54690548+DracarysMurdoc@users.noreply.github.com>
2020-01-27 12:50:16 -0600

    Primera versión

---
+++ BuenasPracticas/BuenasPracticas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuenasPracticas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0, i = 0;
+            string eventos;
+            string src = @"C:\Users\juan.pech\source\repos\BuenasPracticas\Eventos.txt";
+            string[] evento = { };
+            DateTime fecha;
+            StreamReader buffer = new StreamReader(src);
+
+            while ((eventos = buffer.ReadLine()) != null)
+            {
+                evento = eventos.Split(',');
+
+                fecha = DateTime.Parse(evento[1]);
+
+                var result = fecha.CompareTo(DateTime.Today);
+
+                Console.WriteLine();
+                
+                contador++; i=0;
+            }
+            buffer.Close();
+            Console.Read();
+
+
+        }
+    }
+}
